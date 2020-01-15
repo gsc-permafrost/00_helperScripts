1b6123558ab49efac7e90c37220ad4a279a61097
gsc-permafrost <peter.morse@canada.ca>
2020-01-15 16:19:45 -0500

    Added comments and documentation

--- helperFunctions.py
+++ helperFunctions.py
@@ -2,15 +2,19 @@
 Name: helperFunctions
 Author: Ryan Parker
 Created: 2019-12-03
-Purpose:
-"""
+Purpose: A series of small functions to perform basic tasks.
 
+Visit https://realpython.com/python-send-email/ for instructions on how to set up a dummy gmail account to send emails
+with the emailMessage function.
+"""
 
 import smtplib
 import arcpy
 
 
 def emailMessage(receiver, message):
+    # Sends an email to the receiver with the message as the email content.
+    # Useful when scripts take multiple hours to run as they can be monitored remotely
     port = 465
     smtpServer = "smtp.gmail.com"
     sender = "********@gmail.com"  # visit link above for instructions on how to set up a gmail account for this
@@ -26,6 +30,7 @@ def emailMessage(receiver, message):
 
 
 def speak(message):
+    # prints out message in python terminal as well as arcpy window
     print(message)
     arcpy.AddMessage(message)
     return
